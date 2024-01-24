31afb4accf3a8092858a9c8e3c5caed7d1fe8ae9
Caarlos645 <casanco2000@gmail.com>
2024-01-24 12:49:27 +0100

    commit header

---
+++ header-hijo/mf-headder/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mf-headder',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mf-headder';
+}
